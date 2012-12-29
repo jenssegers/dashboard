ab7eca39a0ba9c7ebf70a435297a6f9a7adb1243
Jens Segers <segers.jens@gmail.com>
2012-12-29 15:00:04 +0100

    Removing APC

--- index.php
+++ index.php
@@ -4,17 +4,7 @@
  * Load system
  * -------------------------------------------------------------- */
 require('system.php');
-
-
-/* --------------------------------------------------------------
- * Get an APC cached version of the system object if available
- * -------------------------------------------------------------- */
-if(function_exists('apc_store') && function_exists('apc_fetch') && !apc_exists('dashboard')) {
-    $system = new System;
-    apc_store('dashboard', $system, 10);
-} else {
-    $system = apc_fetch('dashboard');
-}
+$system = new System;
 
 
 /* --------------------------------------------------------------
@@ -37,7 +27,6 @@ $base = rtrim(str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname(__FILE__)), '/'
 
 <h1>
     <?php echo $system->os->hostname; ?>
-    <small id="ext-ip"><? echo $system->os->ip; ?></small>
     <small id="uptime">online for <? echo $system->uptime->days; ?> days, <? echo $system->uptime->hours; ?> hours, <? echo $system->uptime->minutes; ?> minutes </small>
 </h1>
 
