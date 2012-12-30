17a9bcfe366bd60f53398cf148d6155cbd6b2935
Jens Segers <segers.jens@gmail.com>
2012-12-30 01:07:23 +0100

    Added active memory usage

--- components/memory.php
+++ components/memory.php
@@ -19,6 +19,9 @@ class Memory {
                     case 'memfree':
                         $this->free = $item[2];
                         break;
+                    case 'memactive':
+                        $this->active = $item[2];
+                        break;
                 }
             }
 
